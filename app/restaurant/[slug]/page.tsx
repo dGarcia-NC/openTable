ff361940484c357d0b2a04724a9add94a4408792
dGarcia-NC <anthonygarcia833@gmail.com>
2023-09-05 19:47:51 -0700

    Cleanup code with layouts

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import NavBar from "../../components/NavBar"
-import Header from '../[slug]/components/Header'
 import Images from './components/Images'
 import Description from "./components/Description"
 import Rating from "./components/Rating"
@@ -10,28 +8,20 @@ import ReservationCard from "./components/ReservationCard"
 
 export default function RestaurantDetailsPage() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        {/* DESCRIPTION PORTION */}
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavBar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
-        </div>
-        {/* DESCRIPTION PORTION */} {/* RESERVATION CARD PORTION */} {/* RESERVATION
+    <>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
+      </div>
+      {/* DESCRIPTION PORTION */} {/* RESERVATION CARD PORTION */} {/* RESERVATION
     CARD PORTION */}
-      </main>
-    </main>
-
+    </>
   )
 }
