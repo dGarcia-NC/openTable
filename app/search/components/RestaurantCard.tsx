845f03003876604083270f847ab2327d6483ca0b
dGarcia-NC <anthonygarcia833@gmail.com>
2023-09-11 23:15:08 -0700

    Add rating stars

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,6 +1,8 @@
-import { Cuisine, Location, PRICE } from "@prisma/client";
+import { Cuisine, Location, PRICE, Review } from "@prisma/client";
 import Link from "next/link";
 import Price from "../../components/Price";
+import { calulateReviewRatingAverage } from "../../../utils/calculateReviewRatingAverage";
+import Stars from "../../components/Stars";
 
 interface Restaurant {
   id: number;
@@ -10,9 +12,18 @@ interface Restaurant {
   cuisine: Cuisine;
   location: Location;
   slug: string;
+  reviews: Review[];
 }
 
 export default function RestaurantCard({restaurant}: {restaurant: Restaurant}) {
+  const renderRatingText = () => {
+    const rating = calulateReviewRatingAverage(restaurant.reviews)
+
+    if (rating > 4) return 'Awesome'
+    else if (rating <= 4 && rating > 3) return 'Good'
+    else if (rating <= 3 && rating > 0) return 'Average'
+    else ''
+  }
   return (
     <div className="border-b flex pb-5 ml-4">
       <img
@@ -23,8 +34,10 @@ export default function RestaurantCard({restaurant}: {restaurant: Restaurant}) {
       <div className="pl-5">
         <h2 className="text-3xl">{restaurant.name}</h2>
         <div className="flex items-start">
-          <div className="flex mb-2">*****</div>
-          <p className="ml-2 text-sm">Awesome</p>
+          <div className="flex mb-2">
+            <Stars reviews={restaurant.reviews}/>
+          </div>
+          <p className="ml-2 text-sm">{renderRatingText()}</p>
         </div>
         <div className="mb-9">
           <div className="font-light flex text-reg">
